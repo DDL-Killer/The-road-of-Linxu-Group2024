bbce42ec3354d578048db4ac4a0d2f19cb5cd96b
Ithaca <2576080342@qq.com>
2025-10-11 20:15:45 +0800

    25-10-11

--- Helo-algo/queue.c
+++ Helo-algo/queue.c
@@ -76,3 +76,69 @@ int pop(LinkedListQueue *queue){
     queue->queSize--;
     return num;
 }
+
+//基于环形数组实现的队列
+typedef struct{
+    int *nums;  //用于存储队列元素的数组
+    int front;  //队首指针，指向队首元素
+    int queSize;  //尾指针，指向队尾
+    int queCapacity;  //队列容量
+} ArrayQueue;
+
+//构造函数
+ArrayQueue *newArrayQueue(int capacity){
+    ArrayQueue *arrayqueue = (ArrayQueue *)malloc(sizeof(ArrayQueue));
+    arrayqueue->queCapacity = capacity;
+    arrayqueue->nums = (int *)malloc(sizeof(int)*arrayqueue->queCapacity);
+    arrayqueue->front = arrayqueue->queSize = 0;
+    return arrayqueue;
+}
+
+//析构函数
+void delArrayQueue(ArrayQueue *queue){
+    free(queue->nums);
+    free(queue);
+}
+
+//获取队列的容量
+int capacity(ArrayQueue *queue){
+    return queue->queCapacity;
+}
+
+//获取队列的长度
+int size(ArrayQueue *queue){
+    return queue->queSize;
+}
+
+//判断队列是否为空
+bool empty(ArrayQueue *queue){
+    return queue->queSize == 0;
+}
+
+//访问队首元素
+int peek(ArrayQueue *queue){
+    assert(queue->queSize!=0);
+    return queue->nums[queue->front];
+}
+
+//入队
+int push(ArrayQueue *queue,int num){
+    if(queue->queSize == queue->queCapacity){
+        printf("FULL\n");
+        return;
+    }
+    //计算队尾指针，指向队尾索引+1
+    //通过取余操作实现rear越过数组尾部后回到头部
+    int rear = (queue->front+queue->queSize)%queue->queCapacity;
+    queue->nums[rear] = num;
+    queue->queCapacity++;
+}
+
+//出队
+int pop(ArrayQueue *queue){
+    int num = peek(queue);
+    //队首指针向后移动一位，若越过队尾，则返回到数组头部
+    queue->front = (queue->front +1)%queue->queCapacity;
+    queue->queCapacity--;
+    return num;
+}
